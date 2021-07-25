b171195a8db8bd819a276b5ef1c4e37733e7ecfc
annechege <annechege780@gmail.com>
2021-07-26 00:12:04 +0300

    adding post.html create admin

---
+++ hood/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Profile, NeighbourHood, Business,Post
+
+admin.site.register(Profile)
+admin.site.register(NeighbourHood)
+admin.site.register(Business)
+admin.site.register(Post)
