2707746de5d88acbedf199d06f8cc44f38394bcf
annechege <annechege780@gmail.com>
2021-07-26 08:31:51 +0300

    create a .env file

--- hood/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile, NeighbourHood, Business,Post
-
-admin.site.register(Profile)
-admin.site.register(NeighbourHood)
-admin.site.register(Business)
-admin.site.register(Post)
